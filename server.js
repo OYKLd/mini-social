f43c530610086aeff06f9c2cebe8fcc65d92595a
OYKLd <lydieouattara828@gmail.com>
2026-03-08 11:33:43 +0000

    V_04

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -7,10 +8,13 @@ app.use(express.json());
 
 connectDB();
 
+// ROUTES
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
-  res.send("API Mini Social Network");
+  res.send("API running");
 });
 
 app.listen(5000, () => {
-  console.log("Server started on port 5000");
+  console.log("Server running on port 5000");
 });
